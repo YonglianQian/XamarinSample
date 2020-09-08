79be26fd7e9bca6f7088a06eb95a3054c42eb0e3
Abraham Qian <qianyonglian@outlook.com>
2020-09-08 17:04:55 +0800

    tweak1

--- AwesomeApp/AwesomeApp/MainPage.xaml.cs
+++ AwesomeApp/AwesomeApp/MainPage.xaml.cs
@@ -13,9 +13,17 @@ namespace AwesomeApp
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        int count = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"you clicked {count} times";
         }
     }
 }
